5c1dbe7c43b61ce8161b9c90c35d1e84921e2b2c
Matt Biersdorf <matt.biersdorf@gmail.com>
2026-03-24 14:55:39 -0500

    Fix postgres code

--- postgres/server.js
+++ postgres/server.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const { Pool } = require("pg");
+import express from "express";
+import { Pool } from "pg";
+
 const pool = new Pool({
   connectionString: "postgresql://postgres:mysecretpassword@localhost:5432",
 });
@@ -8,25 +9,23 @@ async function init() {
   const app = express();
 
   app.get("/get", async (req, res) => {
-    const client = await pool.connect();
-    const [commentsRes, boardRes] = await Promise.all([
-      client.query(
-        // `SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = ${req.query.search}`
-        "SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = $1",
-        [req.query.search],
-      ),
-      client.query("SELECT * FROM boards WHERE board_id = $1", [
-        req.query.search,
-      ]),
-    ]);
-    res
-      .json({
-        status: "ok",
-        board: boardRes.rows[0] || {},
-        posts: commentsRes.rows,
-      })
-      .end();
-    await client.release();
+    try {
+      const client = await pool.connect();
+      const [commentsRes, boardRes] = await Promise.all([
+        client.query("SELECT * FROM comments NATURAL LEFT JOIN rich_content WHERE board_id = $1", [req.query.search]),
+        client.query("SELECT * FROM boards WHERE board_id = $1", [req.query.search]),
+      ]);
+      res
+        .json({
+          status: "ok",
+          board: boardRes.rows[0] || {},
+          posts: commentsRes.rows,
+        })
+        .end();
+      client.release();
+    } catch (err) {
+      console.error(err);
+    }
   });
 
   const PORT = process.env.PORT || 3000;
